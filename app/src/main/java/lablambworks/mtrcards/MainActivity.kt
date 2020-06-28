a632d395ae78720b119cfe57f4bbcddc9acbdb51
LabLamb <lablambworks@gmail.com>
2020-06-28 20:44:06 +0800

    Setup placeholder project

---
+++ app/src/main/java/lablambworks/mtrcards/MainActivity.kt
@@ -0,0 +1,18 @@
+package lablambworks.mtrcards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import lablambworks.mtrcards.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
